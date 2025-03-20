fd636897f2fa415e6c4fcfa92bc29b4f988879f6
Ilia Bubnov <makebreathe1224@gmail.com>
2025-03-20 18:24:55 +0100

    Done some base styling and structured the welcome page

--- src/views/welcomeView.js
+++ src/views/welcomeView.js
@@ -7,8 +7,24 @@ import { START_QUIZ_BUTTON_ID } from '../constants.js';
 export const createWelcomeElement = () => {
   const element = document.createElement('div');
   element.innerHTML = String.raw`
-    <h1>Welcome</h1>
-    <button id="${START_QUIZ_BUTTON_ID}">start quiz</button>
+        <!-- <img src=""> this is here for a possible icon -->
+        <h1 id="welcome-header">Ultimate Movies & TV Series Quiz</h1>
+        <p>
+        Think you’re a true movie buff or a TV series expert? Test your knowledge with this fun and challenging quiz! From legendary 
+        quotes to award-winning films, see if you can pick the right answers and prove you're the ultimate entertainment fan!
+        </p>
+        <label for="user-name" id="name-label" class="display-block">Enter your name and age to begin</label>
+        <div class="input-container">
+          <input type="text" id="user-name" placeholder="Your name">
+          <input type="number" id="day-of-birth" min="1" max="31" placeholder="Day">
+          <input type="number" id="month-of-birth" min="1" max="12" placeholder="Month">
+          <input type="number" id="year-of-birth" min="1900" placeholder="Year">
+          <button id="${START_QUIZ_BUTTON_ID}" class="display-block">Start the quiz</button>
+        </div>
+        <!-- <p id="age-restriction-p">
+        Uh-oh, you’re still in the ‘no R-rated movies’ club! But don’t worry, you’ll be a movie buff in no 
+        time. Come back when you can stay up past 9 PM without getting caught!
+        </p> this can be used when the age restriction is not met (can be easily implemented later) -->
   `;
   return element;
 };
