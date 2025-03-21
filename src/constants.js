c01aedb814b55c976bbaeaa1f1d1a14a8b999301
A. Zetouny <Zet_Ahmad@hotmail.com>
2025-03-21 01:09:21 +0100

    Question Page styling and logic is 80% completed - checkpoint

--- src/constants.js
+++ src/constants.js
@@ -7,5 +7,10 @@
 
 export const USER_INTERFACE_ID = 'user-interface';
 export const START_QUIZ_BUTTON_ID = 'start-quiz-button';
+
+export const QUESTION_IMAGE_ID = 'question-image';
+export const QUESTION_EXPLANATION_ID = 'question-explanation';
+export const QUESTION_NUMBER_ID = 'question-number';
 export const ANSWERS_LIST_ID = 'answers-list';
+export const SKIP_QUESTION_BUTTON_ID = 'skip-question-button';
 export const NEXT_QUESTION_BUTTON_ID = 'next-question-button';
--- src/views/questionView.js
+++ src/views/questionView.js
@@ -1,5 +1,11 @@
-import { ANSWERS_LIST_ID } from '../constants.js';
-import { NEXT_QUESTION_BUTTON_ID } from '../constants.js';
+import {
+  QUESTION_IMAGE_ID,
+  QUESTION_EXPLANATION_ID,
+  QUESTION_NUMBER_ID,
+  ANSWERS_LIST_ID,
+  SKIP_QUESTION_BUTTON_ID,
+  NEXT_QUESTION_BUTTON_ID,
+} from '../constants.js';
 
 /**
  * Create a full question element
@@ -7,17 +13,34 @@ import { NEXT_QUESTION_BUTTON_ID } from '../constants.js';
  */
 export const createQuestionElement = (question) => {
   const element = document.createElement('div');
-
+  element.id = 'question-page-container';
   // I use String.raw just to get fancy colors for the HTML in VS Code.
   element.innerHTML = String.raw`
-    <h1>${question}</h1>
+  <main>
+    <section id="${QUESTION_IMAGE_ID}">
+      <img src="https://placehold.jp/30px/757575/ffffff/600x1000.jpg?text=Image%20Placeholder&css=%7B%7D" alt="Image Placeholder">
+      <div id="${QUESTION_EXPLANATION_ID}"></div>
+    </section>
+    
+    <section id="question-content">
+      <div id="${QUESTION_NUMBER_ID}"></div>
+      <div id="arrow-icon"><svg version="1.1" viewBox="0 0 512 512" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="M322.7,128.4L423,233.4c6,5.8,9,13.7,9,22.4c0,8.7-3,16.5-9,22.4L322.7,383.6c-11.9,12.5-31.3,12.5-43.2,0  c-11.9-12.5-11.9-32.7,0-45.2l48.2-50.4h-217C93.7,288,80,273.7,80,256c0-17.7,13.7-32,30.6-32h217l-48.2-50.4  c-11.9-12.5-11.9-32.7,0-45.2C291.4,115.9,310.7,115.9,322.7,128.4z"/></svg></div>
+      <div id="question-answer">
+        <div id="question-text">${question}</div>
+        <ul id="${ANSWERS_LIST_ID}"></ul>
+      </div>
+    </section>
+  </main>
 
-    <ul id="${ANSWERS_LIST_ID}">
-    </ul>
-
-    <button id="${NEXT_QUESTION_BUTTON_ID}">
-      Next question
+  <nav>
+    <button id="${SKIP_QUESTION_BUTTON_ID}">
+      Skip Question
     </button>
+
+    <button id="${NEXT_QUESTION_BUTTON_ID}" disabled>
+      Next Question
+    </button>
+  </nav>
   `;
 
   return element;
