e57d0392175a07a411d8391f47ecdb73af6f0b16
Ilia Bubnov <makebreathe1224@gmail.com>
2025-03-21 18:10:02 +0100

    added images representing questions and added a new key imgLink to questions with value representing img location

--- src/data.js
+++ src/data.js
@@ -15,6 +15,7 @@ export const quizData = {
       selected: null,
       explanation:
         'There are 11 installments in the Fast & Furious franchise, including 10 main films (The Fast and the Furious to Fast X) and one spin-off (Hobbs & Shaw)',
+      imgLink: 'src/assets/question-images/fast-and-furious.jpg',
     },
 
     {
@@ -30,6 +31,7 @@ export const quizData = {
       selected: null,
       explanation:
         'Leonardo DiCaprio won his first Academy Award for Best Actor for the movie "The Revenant" (2015) at the 88th Academy Awards in 2016.',
+      imgLink: 'src/assets/question-images/leonardo-dicaprio.jpg'
     },
 
     {
@@ -45,6 +47,7 @@ export const quizData = {
       selected: null,
       explanation:
         'Walter White, played by Bryan Cranston, is the main character in Breaking Bad. He adopts the alias "Heisenberg" as he transforms from a high school chemistry teacher into a feared drug kingpin',
+      imgLink: 'src/assets/question-images/breaking-bad.jpg'
     },
 
     {
@@ -60,6 +63,7 @@ export const quizData = {
       selected: null,
       explanation:
         "Vibranium is a rare and powerful metal in the Marvel Cinematic Universe (MCU). Captain America's shield is made from it, and it originates from the fictional African nation of Wakanda, which plays a major role in Black Panther.",
+      imgLink: 'src/assets/question-images/captain-america.jpg'
     },
 
     {
@@ -74,6 +78,7 @@ export const quizData = {
       selected: null,
       explanation:
         'Winter is Coming is one of the most iconic phrases from Game of Thrones. It is the motto of House Stark and serves as a warning about the harsh winters in Westeros, as well as the coming threats beyond the Wall.',
+      imgLink: 'src/assets/question-images/witcher.jpg'
     },
 
     {
@@ -88,6 +93,7 @@ export const quizData = {
       selected: null,
       explanation:
         'The score for Tenet (2020) was composed by Ludwig Göransson',
+      imgLink: 'src/assets/question-images/tenet.jpg'
     },
 
     {
@@ -102,6 +108,7 @@ export const quizData = {
       selected: null,
       explanation:
         'The modern adaptations of Dune (2021, 2024) and Blade Runner 2049 (2017) were directed by Denis Villeneuve',
+      imgLink: 'src/assets/question-images/dune.jpg'
     },
 
     {
@@ -116,6 +123,7 @@ export const quizData = {
       selected: null,
       explanation:
         'The main song from Cyberpunk: Edgerunners is "I Really Want to Stay at Your House" by Rosa Walton (of the band Let’s Eat Grandma)',
+      imgLink: 'src/assets/question-images/cyberpunk.jpg'
     },
 
     {
@@ -130,6 +138,7 @@ export const quizData = {
       selected: null,
       explanation:
         'The first game in Squid Game was "Red Light, Green Light", where players had to move toward a finish line while a giant robotic doll monitored them',
+      imgLink: 'src/assets/question-images/squid-games.jpg'
     },
 
     {
@@ -144,6 +153,7 @@ export const quizData = {
       selected: null,
       explanation:
         ' The name was revealed in The Mandalorian Season 2, Episode 5, during a conversation between Grogu and Ahsoka Tano',
+      imgLink: 'src/assets/question-images/mandalorian.jpg'
     },
 
     {
@@ -159,6 +169,7 @@ export const quizData = {
       selected: null,
       explanation:
         'The name of the coffee shop where the main characters of Friends often hang out is Central Perk',
+      imgLink: 'src/assets/question-images/friends.jpg'
     },
 
     {
@@ -173,6 +184,7 @@ export const quizData = {
       selected: null,
       explanation:
         'Walter White, the main character from Breaking Bad, has been treated for lung cancer',
+      imgLink: 'src/assets/question-images/breaking-bad.jpg'
     },
   ],
 };
